3d19b6467e332e09cb0b49700021378b1bf1cbb2
Juha Vehvilainen <jusu@pinktwins.com>
2014-12-18 23:58:42 +0200

    nada

--- src/com/pinktwins/elephant/CustomEditor.java
+++ src/com/pinktwins/elephant/CustomEditor.java
@@ -24,7 +24,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.BorderFactory;
-import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
