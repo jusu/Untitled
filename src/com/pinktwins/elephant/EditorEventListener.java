6f8dcba564435d241b7bd31531722a072d3da0ec
Juha Vehvilainen <jusu@pinktwins.com>
2014-12-26 10:37:38 +0200

    EditorEventListener -> separate file.

---
+++ src/com/pinktwins/elephant/EditorEventListener.java
@@ -0,0 +1,19 @@
+package com.pinktwins.elephant;
+
+import java.io.File;
+import java.util.List;
+
+import javax.swing.JTextPane;
+
+// Notify 'NoteEditor' that editing has gained/lost focus,
+// or other editing events happened.
+
+public interface EditorEventListener {
+	public void editingFocusGained();
+
+	public void editingFocusLost();
+
+	public void caretChanged(JTextPane text);
+
+	public void filesDropped(List<File> files);
+}
