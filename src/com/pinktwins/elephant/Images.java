7cba46fe2ef3861926bfc253d991e8b79a991d85
Juha Vehvilainen <jusu@pinktwins.com>
2014-12-30 01:22:34 +0200

    Compiler warnings: custom settings, ignore serialization warnings. Added Images.java.

---
+++ src/com/pinktwins/elephant/Images.java
@@ -0,0 +1,26 @@
+package com.pinktwins.elephant;
+
+import java.awt.Image;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import javax.imageio.ImageIO;
+
+public class Images {
+
+	public static Iterator<Image> iterator(String[] names) {
+		ArrayList<Image> list = new ArrayList<Image>();
+		for (int n = 0; n < names.length; n++) {
+			Image img = null;
+			try {
+				img = ImageIO.read(Sidebar.class.getClass().getResourceAsStream("/images/" + names[n] + ".png"));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			list.add(img);
+		}
+		return list.iterator();
+	}
+
+}
