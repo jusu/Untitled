bd55b4a4b31a630d58682cc38ab41f3606283204
Juha Vehvilainen <jusu@pinktwins.com>
2015-01-22 20:29:43 +0200

    1px

--- src/com/pinktwins/elephant/HintTextField.java
+++ src/com/pinktwins/elephant/HintTextField.java
@@ -33,7 +33,7 @@ public class HintTextField extends JTextField {
 		if (getText().length() == 0) {
 			g.setFont(_font);
 
-			int adjust_y = _font == ElephantWindow.fontMediumPlus ? 2 : 0;
+			int adjust_y = _font == ElephantWindow.fontMediumPlus ? 2 + adjustOS : 0;
 
 			int h = getHeight();
 			((Graphics2D) g).setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
@@ -44,7 +44,7 @@ public class HintTextField extends JTextField {
 			int m = 0xfefefefe;
 			int c2 = ((c0 & m) >>> 1) + ((c1 & m) >>> 1);
 			g.setColor(new Color(c2, true));
-			g.drawString(_hint, ins.left, h / 2 + fm.getAscent() / 2 - 2 + adjust_y + adjustOS);
+			g.drawString(_hint, ins.left, h / 2 + fm.getAscent() / 2 - 2 + adjust_y);
 		}
 	}
 
