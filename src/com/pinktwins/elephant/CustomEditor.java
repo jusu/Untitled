f32c22d6889c8c576afdd45e0755f2b374aa527c
Juha Vehvilainen <jusu@pinktwins.com>
2015-02-11 18:34:29 +0200

    Fixed silly bug on copypasting less than 5 chars.

--- src/com/pinktwins/elephant/CustomEditor.java
+++ src/com/pinktwins/elephant/CustomEditor.java
@@ -510,7 +510,7 @@ public class CustomEditor extends RoundPanel {
 
 					i.pos -= i.adjust;
 
-					if (!"{\\rtf".equals(s.substring(0, 5))) {
+					if (s.length() < 5 || !"{\\rtf".equals(s.substring(0, 5))) {
 						i.doc.insertString(i.pos, s, null);
 					} else {
 						if (isMarkdown) {
