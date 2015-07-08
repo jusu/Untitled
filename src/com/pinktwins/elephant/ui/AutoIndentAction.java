b5d2b14d8f697958635ff855438922a56a80ed5a
Juha Vehvilainen <jusu@pinktwins.com>
2015-07-08 11:54:38 +0300

    AutoIndentAction: pressing enter twice removes the extraneous bullet sign

--- src/com/pinktwins/elephant/ui/AutoIndentAction.java
+++ src/com/pinktwins/elephant/ui/AutoIndentAction.java
@@ -13,95 +13,108 @@ package com.pinktwins.elephant.ui;
  * but WITHOUT ANY WARRANTY; without even the implied warranty of 
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
  * Lesser General Public License for more details. 
- */ 
- 
-import javax.swing.*; 
+ */
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.JTextPane;
+import javax.swing.text.BadLocationException;
 import javax.swing.text.Document;
 import javax.swing.text.Utilities;
-import javax.swing.text.BadLocationException; 
-import java.awt.event.ActionEvent; 
- 
-/** 
- * @author Santhosh Kumar T 
+
+/**
+ * @author Santhosh Kumar T
  * @email santhosh@fiorano.com
  *
- * Updated from JTextArea to JTextPane
- */ 
-public class AutoIndentAction extends AbstractAction { 
-    public void actionPerformed(ActionEvent ae) { 
-        JTextPane comp = (JTextPane)ae.getSource();
-        Document doc = comp.getDocument(); 
- 
-        if(!comp.isEditable()) 
-            return; 
-        try {
-            String str = getTextUntilCursor(comp);
-            String whiteSpace = getLeadingWhiteSpace(str);
-            String strAtCaret = doc.getText(comp.getCaretPosition(), 1);
-            String additionalBullet;
-            if ("*-+".contains(strAtCaret)) {
-                // special case: cursor right before bullet sign --> do not insert additional bullet sign
-                additionalBullet = "";
-            } else {
-                additionalBullet = determineAdditionalBullet(str);
-            }
-            doc.insertString(comp.getCaretPosition(), '\n' + whiteSpace + additionalBullet, null);
-        } catch(BadLocationException ex) { 
-            try { 
-                doc.insertString(comp.getCaretPosition(), "\n", null); 
-            } catch(BadLocationException ignore) { 
-                // ignore 
-            } 
-        } 
-    } 
+ *        Updated from JTextArea to JTextPane
+ */
+public class AutoIndentAction extends AbstractAction {
+	public void actionPerformed(ActionEvent ae) {
+		JTextPane comp = (JTextPane) ae.getSource();
+		Document doc = comp.getDocument();
 
-    /**
-     * Checks if the given string begins with "* ", "- ", or "+ " (after removing the white spaces before)
-     * If yes, that string is returned
-     * If not, "" is returned
-     * @param str
-     * @return
-     */
-    private String determineAdditionalBullet(String str) {
-        String trimmed = str.trim();
-        if (trimmed.startsWith("* ")) {
-            return "* ";
-        } else if (trimmed.startsWith("- ")) {
-            return "- ";
-        } else if (trimmed.startsWith("+ ")) {
-            return "+ ";
-        } else {
-            return "";
-        }
-    }
+		if (!comp.isEditable())
+			return;
+		try {
+			int caretPosition = comp.getCaretPosition();
+			String str = getTextUntilCursor(comp);
+			String whiteSpace = getLeadingWhiteSpace(str);
+			String strAtCaret = doc.getText(caretPosition, 1);
+			String additionalBullet;
+			if ("*-+".contains(strAtCaret)) {
+				// special case: cursor right before bullet sign --> do not insert additional bullet sign
+				additionalBullet = "";
+			} else {
+				additionalBullet = determineAdditionalBullet(str);
+			}
 
-    /**
-     *  Returns leading white space characters in the specified string. 
-     */ 
-    private String getLeadingWhiteSpace(String str) { 
-        return str.substring(0, getLeadingWhiteSpaceWidth(str)); 
-    } 
- 
-    /**
-     *  Returns the number of leading white space characters in the specified string. 
-     */ 
-    private int getLeadingWhiteSpaceWidth(String str) { 
-        int whitespace = 0; 
-        while(whitespace<str.length()) {
-            char ch = str.charAt(whitespace); 
-            if(ch==' ' || ch=='\t') 
-                whitespace++; 
-            else 
-                break; 
-        } 
-        return whitespace; 
-    } 
+			// pressing enter twice removes the extraneous bullet sign
+			if (caretPosition > 1) {
+				String strBeforeCaret = doc.getText(caretPosition - 2, 1);
+				if ("*-+".contains(strBeforeCaret) && str.length() == whiteSpace.length() + 2 && str.charAt(str.length() - 1) == ' ') {
+					doc.remove(caretPosition - whiteSpace.length() - 2, whiteSpace.length() + 2);
+					whiteSpace = "";
+				}
+			}
 
-    private static String getTextUntilCursor(JTextPane comp) throws BadLocationException {
-        int caretPosition = comp.getCaretPosition();
-        int start = Utilities.getRowStart(comp, caretPosition);
-        Document doc = comp.getDocument();
-        String str = doc.getText(start, caretPosition - start);
-        return str;
-    }
-} 
+			doc.insertString(comp.getCaretPosition(), '\n' + whiteSpace + additionalBullet, null);
+		} catch (BadLocationException ex) {
+			try {
+				doc.insertString(comp.getCaretPosition(), "\n", null);
+			} catch (BadLocationException ignore) {
+				// ignore
+			}
+		}
+	}
+
+	/**
+	 * Checks if the given string begins with "* ", "- ", or "+ " (after removing the white spaces before) If yes, that
+	 * string is returned If not, "" is returned
+	 * 
+	 * @param str
+	 * @return
+	 */
+	private String determineAdditionalBullet(String str) {
+		String trimmed = str.trim();
+		if (trimmed.startsWith("* ")) {
+			return "* ";
+		} else if (trimmed.startsWith("- ")) {
+			return "- ";
+		} else if (trimmed.startsWith("+ ")) {
+			return "+ ";
+		} else {
+			return "";
+		}
+	}
+
+	/**
+	 * Returns leading white space characters in the specified string.
+	 */
+	private String getLeadingWhiteSpace(String str) {
+		return str.substring(0, getLeadingWhiteSpaceWidth(str));
+	}
+
+	/**
+	 * Returns the number of leading white space characters in the specified string.
+	 */
+	private int getLeadingWhiteSpaceWidth(String str) {
+		int whitespace = 0;
+		while (whitespace < str.length()) {
+			char ch = str.charAt(whitespace);
+			if (ch == ' ' || ch == '\t')
+				whitespace++;
+			else
+				break;
+		}
+		return whitespace;
+	}
+
+	private static String getTextUntilCursor(JTextPane comp) throws BadLocationException {
+		int caretPosition = comp.getCaretPosition();
+		int start = Utilities.getRowStart(comp, caretPosition);
+		Document doc = comp.getDocument();
+		String str = doc.getText(start, caretPosition - start);
+		return str;
+	}
+}
