f9c3500121d81802edcb2320aaff996c49177f80
Taavi <taavikunto@gmail.com>
2015-09-30 23:59:59 +0300

    JUnit test on Cradle build  issue #176

--- test/com/pinktwins/elephant/MainTest.java
+++ test/com/pinktwins/elephant/MainTest.java
@@ -1,6 +1,9 @@
 package com.pinktwins.elephant;
 
 import org.junit.Test;
+
+import com.pinktwins.elephant.data.Tag;
+
 import static org.junit.Assert.*;
 
 public class MainTest {
@@ -8,6 +11,20 @@ public class MainTest {
         public void test1(){
                 assertEquals("hello world!", "hello world!");
         }
+        
+        @Test
+        public void testTagName(){
+        	Tag tag = new Tag("tag");
+        	assertEquals("tag",tag.name());
+        }
+        
+        /*
+         * failing test (build should show an error)
+         * @Test
+         * public void test2(){
+         * assertEquals("this is","false");
+         * }
+         */
 }
 
 
