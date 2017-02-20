542d4467b8fcc63c8d5f03f8189d2ddbc5d55c06
thomas plorin <thomas@plorin.org>
2017-02-20 19:04:18 +0100

    System.Exit when window close on Linux and Windows

--- src/com/pinktwins/elephant/ElephantWindow.java
+++ src/com/pinktwins/elephant/ElephantWindow.java
@@ -178,6 +178,9 @@ public class ElephantWindow extends JFrame {
 			saveChanges();
 			cleanup();
 			ElephantWindow.this.dispose();
+			if (SystemUtils.IS_OS_WINDOWS || SystemUtils.IS_OS_LINUX) {
+				System.exit(0);
+			}
 		}
 	};
 
