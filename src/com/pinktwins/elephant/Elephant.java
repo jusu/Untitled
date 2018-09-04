9750ef24b8bf40aad56ddf0ad18c3381e015c4c2
Juha Vehvilainen <jusu@pinktwins.com>
2018-09-04 15:02:53 +0300

    bumped version to 49, wip.

--- src/com/pinktwins/elephant/Elephant.java
+++ src/com/pinktwins/elephant/Elephant.java
@@ -16,7 +16,7 @@ import com.pinktwins.elephant.data.Vault;
 
 public class Elephant {
 
-	public static final int VERSION = 48;
+	public static final int VERSION = 49;
 
 	private static final Logger LOG = Logger.getLogger(Elephant.class.getName());
 
