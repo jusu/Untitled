37844312119a47feabd3a0d5c61042d8db362430
Juha Vehvilainen <jusu@pinktwins.com>
2018-09-20 17:36:40 +0300

    v50

--- src/com/pinktwins/elephant/Elephant.java
+++ src/com/pinktwins/elephant/Elephant.java
@@ -16,7 +16,7 @@ import com.pinktwins.elephant.data.Vault;
 
 public class Elephant {
 
-	public static final int VERSION = 49;
+	public static final int VERSION = 50;
 
 	private static final Logger LOG = Logger.getLogger(Elephant.class.getName());
 
